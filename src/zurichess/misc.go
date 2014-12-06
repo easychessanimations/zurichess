f037b82529e7cedcdc3a1964445560ba6f2fe233
Alexandru Moșoi <brtzsnr@gmail.com>
2014-12-06 17:58:24 +0100

    Add LogN & LSB

---
+++ src/zurichess/misc.go
@@ -0,0 +1,18 @@
+package main
+
+// LSB returns the least significant bit of n.
+func LSB(n uint64) uint64 {
+	return n & (-n)
+}
+
+var debrujin64 = [64]uint{
+	0, 1, 2, 7, 3, 13, 8, 19, 4, 25, 14, 28, 9, 34, 20, 40,
+	5, 17, 26, 38, 15, 46, 29, 48, 10, 31, 35, 54, 21, 50, 41, 57,
+	63, 6, 12, 18, 24, 27, 33, 39, 16, 37, 45, 47, 30, 53, 49, 56,
+	62, 11, 23, 32, 36, 44, 52, 55, 61, 22, 43, 51, 60, 42, 59, 58,
+}
+
+// LogN returns the logarithm of n, where n is a power of two.
+func LogN(n uint64) uint {
+	return debrujin64[(n*0x218A392CD3D5DBF)>>58&0x3F]
+}
---
+++ src/zurichess/misc_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestLSB(t *testing.T) {
+	data := []struct {
+		n   uint64
+		lsb uint64
+	}{
+		{1, 1},
+		{13, 1},
+		{24, 8},
+		{16, 16},
+		{17 << 20, 1 << 20},
+	}
+
+	for _, d := range data {
+		actual := LSB(d.n)
+		if d.lsb != actual {
+			t.Errorf("expected LSB(%d) == %d, got %d", d.n, d.lsb, actual)
+		}
+	}
+}
+
+func TestLogN(t *testing.T) {
+	for e := uint(0); e < 64; e++ {
+		n := uint64(1) << e
+		actual := LogN(n)
+		if actual != e {
+			t.Errorf("expected LogN(%d) == %d, got %d", n, e, actual)
+		}
+	}
+}
