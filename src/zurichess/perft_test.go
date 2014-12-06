ed427ca2c6defbede7ad85ca3e52d6ced1a9eb66
Alexandru Moșoi <brtzsnr@gmail.com>
2014-12-06 17:03:19 +0100

    Add castling numbers for perft.

--- src/zurichess/perft_test.go
+++ src/zurichess/perft_test.go
@@ -10,23 +10,26 @@ type counters struct {
 	nodes     uint64
 	captures  uint64
 	enpassant uint64
+	castles   uint64
 }
 
 func (co *counters) Add(ot counters) {
 	co.nodes += ot.nodes
 	co.captures += ot.captures
 	co.enpassant += ot.enpassant
+	co.castles += ot.castles
 }
 
 func (co *counters) Equal(ot counters) bool {
 	return co.nodes == ot.nodes &&
 		co.captures == ot.captures &&
-		co.enpassant == ot.enpassant
+		co.enpassant == ot.enpassant &&
+		co.castles == ot.castles
 }
 
 func perft(pos *Position, depth int) counters {
 	if depth == 0 {
-		return counters{1, 0, 0}
+		return counters{1, 0, 0, 0}
 	}
 
 	co := counters{}
@@ -37,6 +40,9 @@ func perft(pos *Position, depth int) counters {
 		}
 		if mo.MoveType == Enpassant {
 			co.enpassant++
+		}
+		if mo.MoveType == Castling {
+			co.castles++
 		}
 
 		pos.DoMove(mo)
@@ -52,7 +58,6 @@ func testHelper(t *testing.T, fen string, testData []counters) {
 		if err != nil {
 			t.Errorf("invalid FEN: %s", fen)
 		}
-		pos.PrettyPrint()
 
 		actual := perft(pos, depth)
 		if !expected.Equal(actual) {
@@ -64,38 +69,38 @@ func testHelper(t *testing.T, fen string, testData []counters) {
 
 func TestPerftInitial(t *testing.T) {
 	testHelper(t, FENStartPos, []counters{
-		{1, 0, 0},
-		{20, 0, 0},
-		{400, 0, 0},
-		{8902, 34, 0},
-		{197281, 1576, 0},
-		// {4865609, 82719, 0},
-		// {119060324, 2812008, 5248},
+		{1, 0, 0, 0},
+		{20, 0, 0, 0},
+		{400, 0, 0, 0},
+		{8902, 34, 0, 0},
+		{197281, 1576, 0, 0},
+		// {4865609, 82719, 0, 0},
+		// {119060324, 2812008, 5248, 0},
 	})
 
 }
 
 func TestPerftKiwipete(t *testing.T) {
 	testHelper(t, FENKiwipete, []counters{
-		{1, 0, 0},
-		{48, 8, 0},
-		{2039, 351, 1},
-		{97862, 17102, 45},
-		// {4085603, 757163, 1929},
-		// {193690690, 35043416, 73365},
+		{1, 0, 0, 0},
+		{48, 8, 0, 2},
+		{2039, 351, 1, 91},
+		{97862, 17102, 45, 3162},
+		// {4085603, 757163, 1929, 128013},
+		// {193690690, 35043416, 73365, 4993637},
 	})
 }
 
 func TestPerftDuplain(t *testing.T) {
 	fen := "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"
 	testHelper(t, fen, []counters{
-		{1, 0, 0},
-		{14, 1, 0},
-		{191, 14, 0},
-		{2812, 209, 2},
-		{43238, 3348, 123},
-		// {674624, 52051, 1165},
-		// {11030083, 940350, 33325},
-		// {178633661, 14519036, 294874},
+		{1, 0, 0, 0},
+		{14, 1, 0, 0},
+		{191, 14, 0, 0},
+		{2812, 209, 2, 0},
+		{43238, 3348, 123, 0},
+		// {674624, 52051, 1165, 0},
+		// {11030083, 940350, 33325, 0},
+		// {178633661, 14519036, 294874, 0},
 	})
 }
