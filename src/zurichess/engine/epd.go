80aec1b811e7866f4d3d32bc313b51e90b5a7eb1
Alexandru Moșoi <brtzsnr@gmail.com>
2014-12-19 12:06:03 +0100

    epd: Split PositionFromFEN into smaller functions.

--- src/zurichess/engine/epd.go
+++ src/zurichess/engine/epd.go
@@ -30,20 +30,16 @@ var (
 	}
 )
 
-// Parses position from a FEN string.
-// See http://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
-func PositionFromFEN(fen string) (*Position, error) {
-	field := strings.Fields(fen)
-	if len(field) < 4 {
-		return nil, fmt.Errorf("expected at least 4 fields, got %d", len(field))
-	}
+// Extended Position Description
+type EPD struct {
+	Position *Position
+}
 
-	pos := &Position{}
-
+func (epd *EPD) parsePosition(str string) error {
 	// Parse position.
-	ranks := strings.Split(field[0], "/")
+	ranks := strings.Split(str, "/")
 	if len(ranks) != 8 {
-		return nil, fmt.Errorf("expected 8 rows, got %d", len(ranks))
+		return fmt.Errorf("expected 8 rows, got %d", len(ranks))
 	}
 	for r := range ranks {
 		sq := RankFile(7-r, 0) // FEN describes the table from 8th rank.
@@ -53,37 +49,71 @@ func PositionFromFEN(fen string) (*Position, error) {
 				if '1' <= p && p <= '8' {
 					sq = sq.Relative(0, int(p)-int('0')-1)
 				} else {
-					return nil, fmt.Errorf("unhandled '%c'", p)
+					return fmt.Errorf("unhandled '%c'", p)
 				}
 			}
-			pos.Put(sq, pi)
+			epd.Position.Put(sq, pi)
 			sq = sq.Relative(0, 1)
 		}
 	}
+	return nil
+}
 
-	// Parse next to move.
-	switch field[1] {
+func (epd *EPD) parseToMove(str string) error {
+	switch str {
 	case "w":
 		pos.SetToMove(White)
 	case "b":
 		pos.SetToMove(Black)
 	default:
-		return nil, fmt.Errorf("unknown color %s", field[1])
+		return fmt.Errorf("unknown color %s", str)
 	}
+	return nil
+}
 
-	// Parse castling rights.
-	castle := NoCastle
-	for _, p := range field[2] {
-		vastle |= symbolToCastle[p]
+func (epd *EPD) parseCastlingRights(str string) error {
+	if str == "-" {
+		return nil
 	}
-	pos.SetCastle(castle)
+	for _, p := range str {
+		castle := symbolToCastle[p]
+		if castle == NoCastle {
+			return fmt.Errorf("unknown castle rights %s", str)
+		}
+		epd.Position.Castle |= castle
+	}
+	return nil
+}
 
-	// Parse Enpassant.
+func (epd *EPD) parseEnpassant(str string) error {
 	// TODO: handle error
-        if field[3][:1] != "-" {
+        if str[:1] != "-" {
 		pos.SetEnpassant(SquareFromString(field[3]))
+        }
+	return nil
+}
+
+// Parses position from a FEN string.
+// See http://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+func PositionFromFEN(fen string) (*Position, error) {
+	field := strings.Fields(fen)
+	if len(field) < 6 {
+		return nil, fmt.Errorf("expected at least 6 fields, got %d", len(field))
 	}
 
-	// TODO: halfmove, fullmove
-	return pos, nil
+	epd := &EPD{Position: &Position{}}
+	if err := epd.parsePosition(field[0]); err != nil {
+		return nil, err
+	}
+	if err := epd.parseToMove(field[1]); err != nil {
+		return nil, err
+	}
+	if err := epd.parseCastlingRights(field[2]); err != nil {
+		return nil, err
+	}
+	if err := epd.parseEnpassant(field[3]); err != nil {
+		return nil, err
+	}
+
+	return epd.Position, nil
 }
