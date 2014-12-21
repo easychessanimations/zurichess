67e3ff8bb4550cc9f2684116cdd9fcbfbd24e884
Alexandru Moșoi <brtzsnr@gmail.com>
2014-12-21 17:10:46 +0100

    main: Update scripts to use latest version of cutechess-cli.
    Ubuntu's version is quite outdated.
    The new versions solves the bug with frequent 'Connection stalls'.

--- src/zurichess/debug.sh
+++ src/zurichess/debug.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
-#
-# test.sh evaluate two releases of zurichess.
-# Requires cutechess-cli (latest version) in PATH.
-#
-# Usage: ./test.sh release1 release2
-#
-# 2moves_v1.pgn (not included in the repository) is taken Fishtest.
-# Similarly -draw and -resign flags are take from Fishtest.
 
 if [ -z $1 ] || [ -z $2 ]; then
         echo "Usage: $0 release1 release2"
         exit 1
 fi
 
-pgnout=test.$1.vs.$2.pgn
+pgnout=debug$1.vs.$2.pgn
 mv -f $pgnout $pgnout~
-echo "Will write games in $pgnout"
+echo "Will write game in $pgnout"
 
 cutechess-cli \
+        -debug \
         -srand $RANDOM \
         -repeat \
         -recover \
-        -games 500 \
-        -concurrency 2 \
+        -games 1 \
+        -concurrency 1 \
         -resign movecount=3 score=400 \
         -draw movenumber=34 movecount=8 score=20 \
         -openings file=2moves_v1.pgn format=pgn order=random \
