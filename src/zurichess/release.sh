932baa81a9c16261ee873e10ed3ef2fa097f1a98
Alexandru Moșoi <brtzsnr@gmail.com>
2014-12-24 23:02:59 +0100

    main: Modify release script to include the branch name in the version.

--- src/zurichess/release.sh
+++ src/zurichess/release.sh
@@ -4,7 +4,7 @@ version=`git describe --always --tag`
 branch=`git rev-parse --abbrev-ref HEAD`
 when=`date +"%F %T"`
 
-go build -ldflags "-X main.buildVersion '$version' -X main.buildTime '$when'" . || exit 1
+go build -ldflags "-X main.buildVersion '$branch-$version' -X main.buildTime '$when'" . || exit 1
 cp zurichess zc-$branch
 cp zurichess zc-$branch-$version
 
