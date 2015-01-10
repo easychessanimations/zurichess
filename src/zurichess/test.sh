578e232edaffc2664ebe3fda9f7fece89c131558
Alexandru Moșoi <brtzsnr@gmail.com>
2015-01-10 02:01:13 +0100

    test: Include stockfish in tests.

--- src/zurichess/test.sh
+++ src/zurichess/test.sh
@@ -19,15 +19,17 @@ echo "Will write games in $pgnout"
 
 cutechess-cli \
         -srand $RANDOM \
+        -pgnout $pgnout \
         -repeat \
         -recover \
-        -games 5000 \
+        -rounds 5000 \
         -concurrency 2 \
+        -ratinginterval 20 \
+        -sprt elo0=1 elo1=5 alpha=0.05 beta=0.05 \
         -resign movecount=8 score=500 \
         -draw movenumber=40 movecount=8 score=20 \
         -openings file=2moves_v1.pgn format=pgn order=random \
-        -sprt elo0=1 elo1=5 alpha=0.01 beta=0.01 \
-        -pgnout $pgnout \
-        -engine cmd=`pwd`/$1 name=$1 whitepov \
-        -engine cmd=`pwd`/$2 name=$2 whitepov \
+        -engine conf=$1 \
+        -engine conf=$2 \
+        -engine conf=stockfish \
         -each tc=40/15+0.05 proto=uci
