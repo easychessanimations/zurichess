ebbea32066c1b1d6a4019ee1b2c4c7403bba2f04
Alexandru Moșoi <brtzsnr@gmail.com>
2015-01-10 17:34:07 +0100

    puzzle: Add some script for easy testing.

---
+++ src/zurichess/puzzle/all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# ./all.sh is a script to solve several puzzle test files.
+
+cd `dirname $0`
+go build .
+
+./puzzle --quiet --deadline 100ms --input mates/mateIn1.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn2.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn3.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn4.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn5.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn6.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn7.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input mates/mateIn8.epd 2> /dev/null
+
+./puzzle --quiet --deadline 500ms --input bratko_kopec.epd 2> /dev/null
+./puzzle --quiet --deadline 100ms --input ecm.epd 2> /dev/null
+./puzzle --quiet --deadline 500ms --input silent_but_deadly.epd 2> /dev/null
