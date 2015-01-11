f0502d821673c90e676c8c2128ae022e7e42eac7
Alexandru Moșoi <brtzsnr@gmail.com>
2015-01-11 20:34:16 +0100

    puzzle: Update scripts to support arguments.

--- src/zurichess/puzzle/depth.sh
+++ src/zurichess/puzzle/depth.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-# ./all.sh is a script to solve several puzzle test files.
+#!/bin/sh -e
+# ./depth.sh is a script to solve several puzzle test files.
 
 cd `dirname $0`
 go build .
 
-./puzzle --quiet --max_depth 4 --input mates/mateIn1.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn2.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn3.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn4.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn5.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn6.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn7.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input mates/mateIn8.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn1.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn2.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn3.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn4.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn5.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn6.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn7.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input mates/mateIn8.epd 2> /dev/null
 
-./puzzle --quiet --max_depth 4 --input bratko_kopec.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input ecm.epd 2> /dev/null
-./puzzle --quiet --max_depth 4 --input silent_but_deadly.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input bratko_kopec.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input ecm.epd 2> /dev/null
+./puzzle --quiet --max_depth 4 $* --input silent_but_deadly.epd 2> /dev/null
