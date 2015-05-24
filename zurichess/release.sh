7d3b0fa601eb3c47bb9cab82c1ce6aadaa09aa40
Alexandru Moșoi <brtzsnr@gmail.com>
2015-05-24 22:09:20 +0200

    zurichess: Do no include the commit id in the binary name.
    It creates too many unneeded binaries.

--- zurichess/release.sh
+++ zurichess/release.sh
@@ -6,6 +6,4 @@ when=`date +"%F %T"`
 
 go build -ldflags "-X main.buildVersion '$branch-$version' -X main.buildTime '$when'" . || exit 1
 cp zurichess zurichess-$branch
-cp zurichess zurichess-$branch-$version
-
-echo "Built zurichess-$branch-$version at $when"
+echo "Built zurichess-$branch at $when"
