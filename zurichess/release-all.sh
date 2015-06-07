2b3d3c5540a55d906e6b258e38bf151e34695cce
Alexandru Moșoi <brtzsnr@gmail.com>
2015-06-07 12:24:12 +0200

    zurichess: Update the release script.

--- zurichess/release-all.sh
+++ zurichess/release-all.sh
@@ -2,14 +2,13 @@
 
 branch=`git rev-parse --abbrev-ref HEAD`
 release=${branch#release.}
-version=`git describe --always --tag`
 when=`date +"%F %T"`
 
 # make goos goarch extension
 function make {
         GOOS=$1 GOARCH=$2 go build \
                 -a \
-                -ldflags "-X main.buildVersion '$branch-$version' -X main.buildTime '$when'" \
+                -ldflags "-X main.buildVersion '$release' -X main.buildTime '$when'" \
                 -o zurichess-$release-$1-$2$3 \
                 bitbucket.org/brtzsnr/zurichess/zurichess
 }
