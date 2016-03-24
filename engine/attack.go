62ed1389ea1af6aae89400f805a4978848302bcf
Alexandru Moșoi <brtzsnr@gmail.com>
2016-03-24 09:01:24 +0100

    basic: Speedup Bitboard.Has.
    On AMD64, the Go shifts operators produce a series
    of instructions CMPQ/SBBQ/ANDQ to make sure that the shift
    has defined behavior when the counter is larger than
    the bit-width. In C a large counter is undefined behavior
    so the extra instructions are not generated.
    
    To fix this we add a &63 so that the compiler knows that
    the counter is small enough and shouldn't not generate
    the extra machine code. On ARM the &63 operation is redundant.
    
    Speeds up ECM at depth 8 from 40.6s to 40.1s.

--- engine/attack.go
+++ engine/attack.go
@@ -124,7 +124,8 @@ func slidingAttack(sq Square, deltas [][2]int, occupancy Bitboard) Bitboard {
 func spell(magic uint64, bb Bitboard) uint {
 	shift := uint(magic >> 60)
 	mul := magic * uint64(bb)
-	return uint(mul >> ((64 - shift) & 63)) // &63 is to let the compiler know the shift fits 6 bits
+	return uint(mul >> ((64 - shift) & 63))
+	// &63 lets the compiler now that shift fits 6 bits and should not generate CMPQ, SBBQ, ANDQ instructions on amd64
 }
 
 type magicInfo struct {
